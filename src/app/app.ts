207612eb75b6b4883978e71e33caa81b55ad7a82
Stiven <johnstiven40@gmail.com>
2025-08-14 21:00:29 -0500

    commit in case everything break

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { User } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
+
 // el decorador es la metadata de esta clase (la "class App" de abajo)
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS;
+  selectedUserId = "u2";
+  
+  get selectedUser(){
+    return this.users.find((user) => user.id == this.selectedUserId);
+  }
+
+  onSelectUser(id: string){
+    this.selectedUserId = id;
+  }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
+  // "!" means we know it'll have a value
+  // "required: true" hace que me obligue a poner avatar y name en el frontend
+  // @Input() name!: string; //este input seria opcional 
+
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) id!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'users/' + this.avatar;
+  }
+
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
+
 
 }
