811e56f2b494a7ff8fd5c832fe98116b73d12925
Stiven <johnstiven40@gmail.com>
2025-08-18 20:06:21 -0500

    stable preservices

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { TaskInterface } from './task.model';
+import { Card } from '../../shared/card/card';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 import { UserInterface } from './user.model';
+import { Card } from '../shared/card/card';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [Card],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
